aeff3e7549328a3b0a78de8e6e5ffe38e1dbecb3
Ariel Jumba <arieljumba5@gmail.com>
2023-05-13 15:21:50 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+
+import streamlit as st
+import streamlit_authenticator as stauth
+from deta import Deta
+
+st.title('connect db')
+
+
+deta = Deta("d0gzrajtrbv_ADwYbKZ5gZ6Li5Fek93khne69La4T6VC")
+
+#create a database
+db = deta.Base("users_db")
+
+def insert_user(username,name, role, password):
+    #returns user on a succesful creation otherwise raises an error
+    return db.put({"key":username,"name": name, "role": role,"password":password})
+
+
+
+#import database as db
+
+usernames = st.text_input('username')
+names = st.text_input('name')
+roles = st.selectbox('roles', options = ("admin","inputter"))
+passwords = st.text_input('password')
+hashed_passwords = stauth.Hasher(passwords).generate()
+
+if st.button(label='add'):
+    for (username,name, role, hashed_password) in zip(usernames, names, roles, hashed_passwords):
+        insert_user(username,name,role,hashed_password)
